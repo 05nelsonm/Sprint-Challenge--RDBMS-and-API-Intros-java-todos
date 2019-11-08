b49d2e0ffdaf53b829e4552547edb751daf6d0db
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-08 11:54:02 -0500

    generate constructor/getters & setters for User

--- src/main/java/com/zerofivenelsonm/todos/models/User.java
+++ src/main/java/com/zerofivenelsonm/todos/models/User.java
@@ -37,4 +37,45 @@ public class User {
     )
     @JsonIgnoreProperties("users")
     List<Role> roles = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String username, String primaryemail, String password) {
+        this.username = username;
+        this.primaryemail = primaryemail;
+        this.password = password;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    /*public void setUserid(long userid) {
+        this.userid = userid;
+    }*/
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPrimaryemail() {
+        return primaryemail;
+    }
+
+    public void setPrimaryemail(String primaryemail) {
+        this.primaryemail = primaryemail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
