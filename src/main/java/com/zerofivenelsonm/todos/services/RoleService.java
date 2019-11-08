de1076caa0dffc889b78baa786c2e51d9daa0560
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-08 12:57:47 -0500

    stubbed out RoleServices

--- src/main/java/com/zerofivenelsonm/todos/services/RoleService.java
+++ src/main/java/com/zerofivenelsonm/todos/services/RoleService.java
@@ -1,4 +1,10 @@
 package com.zerofivenelsonm.todos.services;
 
+import com.zerofivenelsonm.todos.models.Role;
+
 public interface RoleService {
+
+    Role findRoleById(long id);
+
+    Role save(Role role);
 }
--- src/main/java/com/zerofivenelsonm/todos/services/RoleServiceImplementation.java
+++ src/main/java/com/zerofivenelsonm/todos/services/RoleServiceImplementation.java
@@ -1,4 +1,23 @@
 package com.zerofivenelsonm.todos.services;
 
-public class RoleServiceImplementation {
+import com.zerofivenelsonm.todos.models.Role;
+import com.zerofivenelsonm.todos.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service(value = "roleService")
+public class RoleServiceImplementation implements RoleService {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public Role findRoleById(long id) {
+        return null;
+    }
+
+    @Override
+    public Role save(Role role) {
+        return null;
+    }
 }
