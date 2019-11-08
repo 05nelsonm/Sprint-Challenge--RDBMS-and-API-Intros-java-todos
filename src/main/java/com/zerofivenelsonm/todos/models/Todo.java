704cd494550ac5e4660cf025f67647eae51fe8e2
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-08 11:55:38 -0500

    generate constructor/getters & setters for Todo

--- src/main/java/com/zerofivenelsonm/todos/models/Todo.java
+++ src/main/java/com/zerofivenelsonm/todos/models/Todo.java
@@ -20,4 +20,45 @@ public class Todo {
     @ManyToOne
     @JoinColumn(name = "userid", nullable = false)
     private User user;
+
+    public Todo() {
+    }
+
+    public Todo(String description, Date datestarted, boolean completed) {
+        this.description = description;
+        this.datestarted = datestarted;
+        this.completed = completed;
+    }
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    /*public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }*/
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDatestarted() {
+        return datestarted;
+    }
+
+    public void setDatestarted(Date datestarted) {
+        this.datestarted = datestarted;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
