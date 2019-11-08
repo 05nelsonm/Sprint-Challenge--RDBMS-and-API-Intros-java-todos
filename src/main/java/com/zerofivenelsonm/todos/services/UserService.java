3f0589db82613a5fc7881d2e715e264d01ba67ce
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-08 13:03:31 -0500

    stubbed out UserServices

--- src/main/java/com/zerofivenelsonm/todos/services/UserService.java
+++ src/main/java/com/zerofivenelsonm/todos/services/UserService.java
@@ -1,4 +1,16 @@
 package com.zerofivenelsonm.todos.services;
 
+import com.zerofivenelsonm.todos.models.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<User> findAll();
+
+    User findUserById(long id);
+
+    User save(User user);
+
+    void delete(long id);
 }
--- src/main/java/com/zerofivenelsonm/todos/services/UserServiceImplementation.java
+++ src/main/java/com/zerofivenelsonm/todos/services/UserServiceImplementation.java
@@ -1,4 +1,42 @@
 package com.zerofivenelsonm.todos.services;
 
-public class UserServiceImplementation {
+import com.zerofivenelsonm.todos.models.User;
+import com.zerofivenelsonm.todos.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@Service(value = "userService")
+public class UserServiceImplementation implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findUserById(long id) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public void delete(long id) {
+
+    }
 }
