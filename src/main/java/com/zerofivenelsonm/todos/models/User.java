9f1afa9d99d042284027cadb8604ee387262f6d4
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-08 11:27:50 -0500

    built out initial model for User

---
+++ src/main/java/com/zerofivenelsonm/todos/models/User.java
@@ -0,0 +1,21 @@
+package com.zerofivenelsonm.todos.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String primaryemail;
+
+    @Column(nullable = false)
+    private String password;
+}
